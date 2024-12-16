e54af0d3044906839256e2f8d5dee394e5c434af
Vicente Cartaxo <juniorcartaxo@gmail.com>
2024-12-16 15:20:51 -0300

    fix: Adição da pasta de produção no storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -22,5 +22,12 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+  viteFinal: (config, {configType}) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
